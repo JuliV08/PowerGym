24dfc927074e1d5d71196e03ee978b7641fdb209
juli <villellijulian@gmail.com>
2026-02-15 14:10:45 -0300

    Standard Vercel-Django pattern (api/index.py)

---
+++ powergym/api/index.py
@@ -0,0 +1,4 @@
+from powergym_project.wsgi import app
+
+# Vercel looks for the 'app' variable in the root of the file
+handler = app
--- powergym/build_files.sh
+++ powergym/build_files.sh
@@ -1,7 +1,4 @@
 # build_files.sh
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
-mkdir -p staticfiles_build
-cp -r static/* staticfiles_build/ 2>/dev/null || :
-cp -r staticfiles/* staticfiles_build/ 2>/dev/null || :
 
