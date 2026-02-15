a5b98fcb0bb3acfc30a56b289490836ef519cede
juli <villellijulian@gmail.com>
2026-02-15 13:58:50 -0300

    Vercel routing fixes (rewrites) and runtime.txt

--- powergym/build_files.sh
+++ powergym/build_files.sh
@@ -1,6 +1,7 @@
 # build_files.sh
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
-mkdir -p staticfiles_build/static
-cp -r static/* staticfiles_build/static/ 2>/dev/null || :
+mkdir -p staticfiles_build
+cp -r static/* staticfiles_build/ 2>/dev/null || :
+cp -r staticfiles/* staticfiles_build/ 2>/dev/null || :
 
