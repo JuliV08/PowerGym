d17da9634acd665ae3661c2acdc0e0253915682f
juli <villellijulian@gmail.com>
2026-02-18 13:25:38 -0300

    cambio de carpetas para setup vercel

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,8 @@
 # build_files.sh
 # Ensure we are in the project directory
-cd powergym || echo "Already in root or powergym dir"
+# build_files.sh
+# Ensure we are in the project directory
+# cd powergym || echo "Already in root or powergym dir"
 
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear --verbosity 2
