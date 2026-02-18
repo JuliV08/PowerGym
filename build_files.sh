97b7c116be88fabc0777ebbcd399f8ac12689b9e
juli <villellijulian@gmail.com>
2026-02-18 13:33:09 -0300

    crucemos los dedos

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,15 @@
 # Ensure we are in the project directory
 # cd powergym || echo "Already in root or powergym dir"
 
+
+# Create and activate a virtual environment
+python3.12 -m venv build_venv
+source build_venv/bin/activate
+
+# Install dependencies into the virtual environment
 pip install -r requirements.txt
+
+# Run collectstatic using the virtual environment's python
 python3 manage.py collectstatic --noinput --clear --verbosity 2
 
 
